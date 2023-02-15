185519899deae481ed855f2a71dc77b65d89097d
949075461 <949075461@qq.com>
2023-02-16 01:13:13 +0800

    init

---
+++ src/test/java/com/yu/chatliteserver/ChatLiteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.chatliteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLiteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
